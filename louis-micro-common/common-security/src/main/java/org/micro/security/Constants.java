cb4d3fc0a6700b41e26c4dc1128e5ab924726881
MrEric125 <627460581@qq.com>
2019-12-14 12:54:50 +0800

    授权登录接口

---
+++ louis-micro-common/common-security/src/main/java/org/micro/security/Constants.java
@@ -0,0 +1,19 @@
+package org.micro.security;
+
+/**
+ * @author John·Louis
+ * @date create in 2019/12/14
+ * description:
+ */
+public interface Constants {
+
+    String LOGINPAGE = "/login";
+
+    String LOGPROCESSPAGE = "/login/process";
+
+    String TOKEN = "/oauth/token";
+
+    String AUTHORIZE = "/oauth/authorize";
+
+
+}
